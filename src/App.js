10e8265edb18e399d2569b954b1d6f64dbe46cda
DeeP-Sad <obscurealien47@gmail.com>
2022-10-10 12:22:51 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <div className="m-auto p-4 w-1/4 bg-slate-500 text-blue-600 items-center hover:bg-red-500">
+        <div className="hover:font-serif bg-black text-2xl w-fit m-auto ">
+          Hi there
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
