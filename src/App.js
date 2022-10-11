ed2953460b8bc5f646ee86522c16aaeab9f16d44
DeeP-Sad <obscurealien47@gmail.com>
2022-10-11 21:25:23 +0530

    created component/first Nav commit

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
 import logo from "./logo.svg";
 import "./App.css";
+import "./Components/Navbar";
+import Navbar from "./Components/Navbar";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Navbar></Navbar>
+    </div>
+  );
 }
 
 export default App;
---
+++ src/Components/Navbar.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <div>
+      <ul>
+        <li>Home</li>
+        <li>Research</li>
+        <li>Publications</li>
+      </ul>
+    </div>
+  );
+};
+
+export default Navbar;
