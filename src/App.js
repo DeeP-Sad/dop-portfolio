67d5b691de0babcd71f51ecbac1f9e950f4cacf1
DeeP-Sad <obscurealien47@gmail.com>
2022-10-11 21:13:21 +0530

    blank

--- src/App.js
+++ src/App.js
@@ -2,18 +2,7 @@ import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <div className="m-auto p-4 w-1/4 bg-slate-500 text-blue-600 items-center hover:bg-red-500">
-        <div className="hover:font-serif bg-black text-2xl w-fit m-auto ">
-          Hi there
-        </div>
-        <div className="text-white text-2xl font-bold font-sans">
-           NO
-        </div>
-      </div>
-    </div>
-  );
+  return <div className="App"></div>;
 }
 
 export default App;
