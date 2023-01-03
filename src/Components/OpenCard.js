a92b65f83caecab9d205a3c24f3e28c61b3891e0
rishabh-hub <rishabh.1056@gmail.com>
2023-01-03 21:39:51 +0530

    working on openings page

--- src/Components/OpenCard.js
+++ src/Components/OpenCard.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const OpenCard = () => {
   return (
-    <div className='bg-[#D9D9D9] rounded-lg overflow-auto'>
+    <div className='bg-[#D9D9D9] rounded-lg h-2/4 md:h-96 overflow-y-auto'>
     <div className='flex flex-col'>
     {/* <div className='basis-1/4 flex-initial '>
     </div> */}
@@ -19,41 +19,42 @@ const OpenCard = () => {
      himenaeos. Curabitur tempus urna at turpis condimentum lobortis.  */}
     </p>
     </div>
-    <div className='flex-auto shrink h-36 mb-3 mt-5 px-4 mx-4 text-black'>
-    <p className=' text-3xl'>
+    <div className='flex-auto mb-3 mt-5 px-4 mx-4 text-black'>
+    <p className=' text-2xl md:text-3xl'>
         Objectives
     </p>
-    <p className=' text-sm ml-1 leading-7 mt-4 pb-5 font-normal font-sans'>
+    <p className=' text-sm ml-1 leading-5 md:leading-7 mt-4 pb-5 font-normal font-sans'>
     <ul className=' list-disc'>
-        <li>Develop a Rubik’s Cube Model (RCM) based
+        <li className='mb-2'>Develop a Rubik’s Cube Model (RCM) based
             simulation technique to understand the folding, 
             unfolding and refolding kinetics and various protein
             folding pathways of Severe Acute Respiratory Syndrome
             Coronavirus-2 (SARS-COV2) spike glycoprotein.
         </li>
-        <li>Study of protein-folding dynamics of spike glycoprotein with
+        <li className='mb-2'>Study of protein-folding dynamics of spike glycoprotein with
             statistical energy landscape theory (SELT) based stochastic model.
         </li>
-        <li>Compare the similarity and dissimilarity between SELT and RCM model
+        <li className='mb-2'>Compare the similarity and dissimilarity between SELT and RCM model
              in details to understand the formation of various types of spike 
              glycoprotein conformations and the efficiency of the Rubik’s Cube model.
         </li>
-        <li>Calculation of the mean first-passage time (MFPT) using both SELT and 
+        <li className='mb-2'>Calculation of the mean first-passage time (MFPT) using both SELT and 
             RCM model at different temperature. 
         </li>
-        <li>Understand the thermodynamics of molecular interaction between spike 
+        <li className='mb-2'>Understand the thermodynamics of molecular interaction between spike 
             glycoprotein and the newly obtained equilibrated folded-unfolded-refolded
             conformations with receptor of angiotensin-converting enzyme-2 (ACE-2) using
             Grand Canonical Monte Carlo (GCMC) Simulation and calculation of the binding energy.
         </li>
+        
     </ul>
     </p>
     </div>
-    <div className='flex-auto rounded-lg shrink h-10 w-2/3 mx-auto my-auto mt-2 mb-3 px-4 bg-[#828282] text-white'>
+    {/* <div className='flex-auto rounded-lg shrink h-10 w-2/3 mx-auto my-auto mt-2 mb-3 px-4 bg-[#828282] text-white'>
     <button className='h-full w-full text-center text-sm font-semibold font-sans'>
       Read more
       </button>
-    </div>
+    </div> */}
     </div>
     </div>
   )
--- src/Components/Openings.js
+++ src/Components/Openings.js
@@ -3,12 +3,12 @@ import OpenCarousal from './OpenCarousal'
 
 const Openings = () => {
   return (
-    <div className="text-white p-10 font-medium text-2xl">
+    <div className="text-white p-10 font-medium text-2xl h-full">
     {/* block div to keep a fixed distance from the fixed navbar */}
     <div className="block h-10 bg-black">
     </div>
       Openings
-      <div className=' w-11/12 relative mx-auto mt-10'>
+      <div className=' md:w-11/12 relative mx-auto mt-10'>
       <OpenCarousal/>
       </div>
   </div>
