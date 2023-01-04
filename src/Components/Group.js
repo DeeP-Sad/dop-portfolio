b6ce95685253f4d52a578098e904f18a7798a9f9
theadityathepanda <adityaamiyapanda27@gmail.com>
2023-01-04 19:21:50 +0530

    Commit message

--- src/Components/Group.js
+++ src/Components/Group.js
@@ -7,7 +7,32 @@ const Group = () => {
       <div className="block h-10 bg-black">
       </div>
       <div>
-        Rishabh abc
+      <div class="grid grid-cols-3 mt-10"  >
+        <div class="bg-indigo-500 hover:bg-indigo-700 text-lg text-white text-center rounded-xl mx-5 my-5">
+          <h1 class="my-20">
+            PhD Dissertation: Ongoing</h1>
+           </div>
+           <div class="bg-indigo-500 hover:bg-indigo-700 text-lg text-white text-center rounded-xl mx-5 my-5">
+          <h1 class="my-20">
+            Research Fellow: Ongoing</h1>
+           </div>
+           <div class="bg-indigo-500 hover:bg-indigo-700 text-lg text-white text-center rounded-xl mx-5 my-5">
+          <h1 class="my-20">
+            M.Tech Dissertation: Awarded</h1>
+           </div>
+           <div class="bg-indigo-500 hover:bg-indigo-700 text-lg text-white text-center rounded-xl mx-5 my-5">
+          <h1 class="my-20">
+            B.Tech Projects: Awarded</h1>
+           </div>
+           <div class="bg-indigo-500 hover:bg-indigo-700 text-lg text-white text-center rounded-xl mx-5 my-5">
+          <h1 class="my-20">
+            B.Tech Projects: Ongoing</h1>
+           </div>
+           <div class="bg-indigo-500 hover:bg-indigo-700 text-lg text-white text-center rounded-xl mx-5 my-5">
+          <h1 class="my-20">
+            Others</h1>
+           </div>
+      </div>
       </div>
         Group
     </div>
